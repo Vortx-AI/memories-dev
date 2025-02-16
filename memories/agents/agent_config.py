70efbb05493e6ed7f44df6ed9289ee753c5e9eba
kumari-jaya <jaya2424@gmail.com>
2025-02-16 16:13:08 +0530

    L1 response

--- memories/agents/agent_config.py
+++ memories/agents/agent_config.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 from gensim.models import KeyedVectors
 import faiss
 import numpy as np
-from memories.core.model import LoadModel
+from memories.models.load_model import LoadModel
 from memories.data_acquisition.data_connectors import parquet_connector
 import requests
 import zipfile
