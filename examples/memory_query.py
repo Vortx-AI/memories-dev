e516a96e23917eec212c5bb7de663b882c82f4f7
kuamri-jaya <jaya2424@gmail.com>
2025-03-04 11:51:02 +0530

    memory query api configuration

--- examples/memory_query.py
+++ examples/memory_query.py
@@ -28,7 +28,7 @@ load_dotenv(env_path)
 logger.info(f"Loading environment from {env_path}")
 
 # Set default API key - using a secure hexadecimal key
-DEFAULT_API_KEY = os.getenv('MEMORY_API_KEY', 'a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6')
+DEFAULT_API_KEY = os.getenv('MEMORY_API_KEY', '')
 os.environ['MEMORY_API_KEY'] = DEFAULT_API_KEY
 
 from memories.models.load_model import LoadModel
